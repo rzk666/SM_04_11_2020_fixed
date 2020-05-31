6316e3a9e5783eec0631905f03f905912279e1c7
Razi <razi@journeyo.com>
2020-06-01 01:50:53 +0300

    [WIP] Splash screen done

--- src/components/Splash/Splash.jsx
+++ src/components/Splash/Splash.jsx
@@ -1,19 +1,78 @@
+/* eslint-disable no-await-in-loop */
 import React from 'react';
+// Util
+import { motion } from 'framer-motion';
 import { connect } from 'react-redux';
 // Styles
 import styles from './Splash.module.scss';
 
-const AnimatedCols = () => (
-  <div className={styles.animated_cols_wrapper}>
-    <div className={styles.col} />
-    <div className={styles.col} />
-    <div className={styles.col} />
-  </div>
-);
+const AnimatedCols = ({ animate }) => {
+  const { animate1, animate2, animate3 } = animate;
+  return (
+    <div className={styles.animated_cols_wrapper}>
+      <motion.div transition={animate1.transition} initial={animate1.initial} animate={animate1.animation} className={styles.col} />
+      <motion.div transition={animate2.transition} initial={animate2.initial} animate={animate2.animation} className={styles.col} />
+      <motion.div transition={animate3.transition} initial={animate3.initial} animate={animate3.animation} className={styles.col} />
+    </div>
+  );
+};
 
 const getImage = (asset, imgName) => asset.filter((img) => img.includes(imgName))[0];
 
 const Splash = (props) => {
+  const animate = {
+    animate1: {
+      initial: {
+        opacity: 0.5,
+        y: 60,
+      },
+      animation: {
+        opacity: [0.5, 1, 0.5],
+        y: [60, 0, 60],
+      },
+      transition: {
+        duration: 1.2,
+        ease: 'easeIn',
+        loop: Infinity,
+        repeatDelay: 0,
+      },
+    },
+    animate2: {
+      initial: {
+        opacity: 0.5,
+        y: 60,
+      },
+      animation: {
+        opacity: [0.5, 1, 0.5],
+        y: [60, 0, 60],
+      },
+      transition: {
+        delay: 0.2,
+        duration: 1.2,
+        ease: 'easeIn',
+        loop: Infinity,
+        repeatDelay: 0,
+      },
+    },
+    animate3: {
+      initial: {
+        opacity: 0.5,
+        y: 60,
+      },
+      animation: {
+        opacity: [0.5, 1, 0.5],
+        y: [60, 0, 60],
+      },
+      transition: {
+        delay: 0.4,
+        duration: 1.2,
+        ease: 'easeIn',
+        loop: Infinity,
+        repeatDelay: 0,
+      },
+    },
+  };
+
   // Get images
   const { assets } = props;
   const { splash, isLoading } = assets;
@@ -46,7 +105,9 @@ const Splash = (props) => {
           src={beatem}
           alt="beatem_logo_wide"
         />
-        <AnimatedCols />
+        <AnimatedCols
+          animate={animate}
+        />
       </>
       )}
     </div>
