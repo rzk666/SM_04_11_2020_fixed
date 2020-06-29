c2e83f59b2755b1e38040be17e32922fcf8aee9f
Razi <razi@journeyo.com>
2020-06-30 00:30:11 +0300

    [WIP] Allah

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,13 +3,30 @@ import React from 'react';
 import BeatemHomeTitle from '../../static/images/icons/beatemhometitle.png';
 import Home from '../../static/images/icons/home.svg';
 import Menu from '../../static/images/icons/menu.svg';
+import { motion, useAnimation } from 'framer-motion';
+// Util
+import classnames from 'classnames';
 // Styles
 import styles from './Header.module.scss';
 
-const Header = ({ auth }) => {
+const Header = ({ auth, currentSport }) => {
   const { isLoggedIn } = auth;
+  const controls = useAnimation();
+  controls.start({
+    opacity: [0, 1],
+    transiton: { duration: 0.3, ease: 'easeIn' },
+  });
   return (
-    <div className={styles.header_container}>
+    <motion.div
+      animate={controls}
+      className={classnames(styles.header_container, {
+        [styles.soccer]: currentSport === 'soccer',
+        [styles.basketball]: currentSport === 'basketball',
+        [styles.baseball]: currentSport === 'baseball',
+        [styles.football]: currentSport === 'football',
+        [styles.hockey]: currentSport === 'hockey',
+      })}
+    >
       <img
         src={BeatemHomeTitle}
         alt="TITLE_HOME"
@@ -30,7 +47,7 @@ const Header = ({ auth }) => {
           className={styles.menu}
         />
       </div>
-    </div>
+    </motion.div>
   );
 };
 
--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 // Components
 import SportsBar from '../components/Home/SportsBar';
+// Images
+// import RightArrow from '../static/images/icons/rightarrow.svg';
+// Util
+import classnames from 'classnames';
 // Styles
 import styles from './HomeView.module.scss';
 
+// ----- App Components ----- //
+const Card = ({ type, onClick }) => {
+  const textTitle = type === 'create'
+    ? 'Create League'
+    : 'Join League';
+  const textBody = type === 'create'
+    ? 'Create your own league and challange your friends'
+    : 'Join a friend\'s league and show \'em who\'s the boss';
+  return (
+    <div className={styles.card_container}>
+      <div className={styles.divider} />
+      <div className={styles.text_container}>
+        <div className={styles.text_title}>{textTitle}</div>
+        {textBody}
+      </div>
+      <div className={styles.right_arrow}>{'>'}</div>
+    </div>
+  );
+};
 
 const HomeView = ({
   login,
@@ -12,6 +35,15 @@ const HomeView = ({
 }) => (
   <div className={styles.wrapper}>
     <SportsBar currentSport={currentSport} changeSport={(sport) => changeSport(sport)} />
+    <div className={styles.top_banner}>
+      <div className={classnames(styles.banner_title)}>
+        JOIN OR CREATE YOUR OWN LEAGUES!
+        <div className={styles.buttons_container}>
+          <Card type="create" />
+          <Card type="join" />
+        </div>
+      </div>
+    </div>
   </div>
 );
 
